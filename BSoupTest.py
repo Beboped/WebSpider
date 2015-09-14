ade3c417593d1b8e114da56ac1fcca2cd6a1478e
Brian Johnson <bjohn31595@gmail.com>
2015-09-14 10:54:47 -0700

    It works! Also builds a page_parent list for tracking down bad links

--- BSoupTest.py
+++ BSoupTest.py
@@ -11,6 +11,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup, SoupStrainer
 import re
 from urllib.error import HTTPError
+from collections import defaultdict
 
 def get_links(url):
     """Takes a url and returns a set with all the links present on the page"""
@@ -23,10 +24,11 @@ def get_links(url):
 urls_encountered = {"http://datasciencedojo.com/"}
 urls_checked = set()
 error_list = []
+page_parents = defaultdict(set)
 ii = 0
 while len(urls_encountered) > 0:
     ii += 1
-    if ii > 200:
+    if ii > 500:
         break
     else:
         nexturl = urls_encountered.pop()    
@@ -39,10 +41,10 @@ while len(urls_encountered) > 0:
         print(e)
     else:
         for tag in new_links:
+            page_parents[tag['href']].add(nexturl)
             if (tag['href'] not in urls_checked 
-                and re.search(r"(\.pdf$|\.jpg$|\.png$)",
+                and re.search(r"wp-content",
                              tag['href'])) is None:
                 urls_encountered.add(tag['href'])
 
 print(error_list)
-print(urls_checked)
